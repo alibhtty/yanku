bae97610781735e4478a9384473786039cc8521e
alibhtty <77213825+alibhtty@users.noreply.github.com>
2024-10-09 03:38:12 +0200

    1.1.2

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,7 +3,6 @@
 // @alibhtty
 
 $ = jQuery;
-
     /// ADD CLASS TO HEADER ON SCROLL ///
     $(window).on("scroll", function() {
         if ($(window).scrollTop() > 70) {
@@ -39,3 +38,32 @@ $ = jQuery;
         this.classList.toggle("clickMenuSeven");
     }
     menuSeven.addEventListener('click', addClassFunSeven);
+
+
+
+
+// Swipe botón Instagram
+    const button = document.querySelector('.make-btn-main');
+    let startX = 0;
+    let currentX = 0;
+
+    button.addEventListener('touchstart', function(event) {
+        startX = event.touches[0].clientX;
+        button.classList.remove('swipe-out');
+        button.querySelector('.carta').classList.add('active-blur');
+    });
+
+    button.addEventListener('touchmove', function(event) {
+        currentX = event.touches[0].clientX - startX;
+        button.style.transform = `translateX(${currentX}px)`;
+    });
+
+    button.addEventListener('touchend', function() {
+        button.querySelector('.carta').classList.remove('active-blur');
+        if (Math.abs(currentX) > button.offsetWidth / 2) {
+            button.classList.add('swipe-out');
+            button.style.transform = currentX > 0 ? `translateX(${window.innerWidth}px)` : `translateX(-${window.innerWidth}px)`;
+        } else {
+            button.style.transform = 'translateX(0)';
+        }
+    });
